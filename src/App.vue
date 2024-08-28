844430f47773ccfc6a5766ae062384f9b7fc2458
Roya <a78945612385238@gmail.com>
2024-08-29 01:38:51 +0800

    feat: Init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AtomicSelect from '@/components/atomics/AtomicSelect.vue';
+</script>
+
+<template>
+  <div class="flex min-h-screen w-full items-center justify-center">
+    <AtomicSelect />
+    <!-- lorem2000 -->
+  </div>
+</template>
---
+++ src/components/atomics/AtomicSelect.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Dropdown } from 'floating-vue'
+
+import 'floating-vue/dist/style.css'
+</script>
+
+<template>
+  <Dropdown
+  :shown="true"
+  :auto-size="true"
+  :distance="6"
+>
+ <div class="min-w-20 min-h-12 bg-red-500 text-white flex items-center justify-center">{{ 'good' }}</div>
+ <template #popper="{ hide }">
+
+  <ul class="w-full flex flex-col max-h-[120px]">
+    <li class="w-full bg-red-400 hover:bg-red-300 text-white" v-for="n in 10" :key="n" @click="hide">
+      {{ n }}
+    </li>
+  </ul>
+ </template>
+</Dropdown>
+</template>
