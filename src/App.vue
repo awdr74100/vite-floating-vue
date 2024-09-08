fd504a161063d8fcf26742e8a8fab487b04dd7cd
Roya <a78945612385238@gmail.com>
2024-09-09 01:04:27 +0800

    feat: add AtomicCheckbox

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,24 @@
 import { useForm, useField, useFieldArray } from '@vorms/core';
 import { valibotResolver } from '@vorms/resolvers/valibot';
 import * as v from 'valibot';
+import AtomicCheckbox from '@/components/atomics/AtomicCheckbox.vue';
+
+const schema = v.object({
+  colors: v.pipe(v.array(v.string()), v.minLength(1)),
+});
+
+const { handleSubmit } = useForm({
+  reValidateMode: 'change',
+  validate: valibotResolver(schema),
+  initialValues: {
+    colors: [],
+  },
+  onSubmit(data) {
+    console.log(data);
+  },
+});
+
+const { value, attrs, error } = useField<string[]>('colors')
 
 // const schema = z.object({
 //   title: z.string().min(6),
@@ -17,41 +35,61 @@ import * as v from 'valibot';
 //   ),
 // });
 
-const schema = v.object({
-  title: v.pipe(v.string(), v.minLength(6)),
-  users: v.pipe(
-    v.array(
-      v.object({
-        name: v.pipe(v.string(), v.minLength(3)),
-        age: v.pipe(v.number(), v.minValue(0)),
-      }),
-    ),
-    v.minLength(0)
-  ),
-});
+// const schema = v.object({
+//   title: v.pipe(v.string(), v.minLength(6)),
+//   users: v.pipe(
+//     v.array(
+//       v.object({
+//         name: v.pipe(v.string(), v.minLength(3)),
+//         age: v.pipe(v.number(), v.minValue(0)),
+//       }),
+//     ),
+//     v.minLength(0)
+//   ),
+// });
 
-const { handleSubmit, errors } = useForm<v.InferOutput<typeof schema>>({
-  initialValues: {
-    title: '',
-    users: [],
-    // users: [{ age: -1, name: '123' }],
-  },
-  validate: valibotResolver(schema),
-  reValidateMode: 'input',
-  onSubmit(data) {
-    console.log(data);
-  },
-});
+// const { handleSubmit, errors } = useForm<v.InferOutput<typeof schema>>({
+//   initialValues: {
+//     title: '',
+//     users: [],
+//     // users: [{ age: -1, name: '123' }],
+//   },
+//   validate: valibotResolver(schema),
+//   reValidateMode: 'input',
+//   onSubmit(data) {
+//     console.log(data);
+//   },
+// });
 
-const { attrs, value, error } = useField('title');
+// const { attrs, value, error } = useField('title');
 
-const { fields, append, remove } = useFieldArray<{ name: string; age: number }>('users');
+// const { fields, append, remove } = useFieldArray<{ name: string; age: number }>('users');
 </script>
 
 <template>
-  <h1 class="text-center text-yellow-500">{{ errors }}</h1>
+  <!-- <h1 class="text-center text-yellow-500">{{ errors }}</h1> -->
   <div class="mx-auto max-w-screen-md">
     <form @submit="handleSubmit">
+      <p>{{ value }}</p>
+      <p>{{ error }}</p>
+      <!-- <AtomicCheckbox name="colors" value="red" />
+      <AtomicCheckbox name="colors" value="blue" />
+      <AtomicCheckbox name="colors" value="yellow" /> -->
+      <div>
+        <label for="red">RED</label>
+        <input id="red" type="checkbox" value="red" v-model="value" v-bind="attrs" />
+      </div>
+      <div>
+        <label for="blue">BLUE</label>
+        <input id="blue" type="checkbox" value="blue" v-model="value" v-bind="attrs" />
+      </div>
+      <div>
+        <label for="purple">PURPLE</label>
+        <input id="purple" type="checkbox" value="purple" v-model="value" v-bind="attrs" />
+      </div>
+      <button type="submit" class="border border-gray-500">SUBMIT</button>
+    </form>
+    <!-- <form @submit="handleSubmit">
       <div class="flex flex-col gap-y-4">
         <input type="text" v-model="value" v-bind="attrs" class="border border-gray-400" />
         <span class="text-red-500">{{ error }}</span>
@@ -92,7 +130,7 @@ const { fields, append, remove } = useFieldArray<{ name: string; age: number }>(
       </button>
 
       <button type="submit" class="bg-green-500 px-3 py-2">Submit</button>
-    </form>
+    </form> -->
   </div>
   <!-- <img :src="Banner" />
   <img src="/banner.jpg" />
---
+++ src/components/atomics/AtomicCheckbox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useField } from '@vorms/core'
+import { toRef } from 'vue';
+
+
+
+type AtomicCheckboxProps = {
+  name: string
+  value: string | number
+}
+
+const props = withDefaults(defineProps<AtomicCheckboxProps>(), {})
+
+const nameRef = toRef(props, 'name')
+
+const { value:checkValue,attrs,error } = useField(nameRef)
+</script>
+
+<template>
+  <div>
+      <label :for="name">{{ name.toLocaleUpperCase() }}</label>
+      <input :id="name" type="checkbox" :value="value" v-model="checkValue" v-bind="attrs">
+      <p>{{ error }}</p>
+    </div>
+</template>
